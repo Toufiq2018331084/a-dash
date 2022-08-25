31d45b3cc199d2501a10b3a733b631994dc2605f
Toufiq2018331084 <52928065+Toufiq2018331084@users.noreply.github.com>
2022-08-25 11:54:51 +0600

    i dont know

--- src/App.js
+++ src/App.js
@@ -7,8 +7,43 @@ import './App.css';
 
 
 export const App = () => {
+  const activeMenu = false;
   return (
-    <h1>slkdjalkwdlk</h1>
+    <div>
+      <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className='fixed right-4 bottom-4' style={{zIndex:'1000'}}>
+          <TooltipComponent content="Settings" position="top">
+            <button type="button"
+            className="text-3xl p-3
+            hover:drop-shadow-xl
+            hover:bg-light-gray text-white"
+            style={{background:'blue', borderRadius:'50%'}}>
+              <FiSettings/>
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu?(
+          <div className='w-72 fixed sidebar
+          dark:bg-secondary-dark-bg
+          bg-white'>
+            Sidebar
+            </div>
+        ):(
+          <div className='w=0
+          dark:bg-secondary-dark-bg'>
+            Sidebar
+          </div>
+        )}
+        <div className={
+          activeMenu? "dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full"
+          : "dark:bg-main-bg bg-main-bg min-h-screen w-full  flex-2"
+        }>
+
+        </div>
+      </div>
+      </BrowserRouter>
+    </div>
   )
 }
 export default App
